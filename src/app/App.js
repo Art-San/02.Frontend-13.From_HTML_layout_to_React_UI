d6c7ffae0118406a43fa69e0948c18fbede30b56
Aleksander <asava@sibmail.com>
2023-04-25 12:54:39 +0700

    все отображ, можно с этим идти в 02-15. Firebase

--- src/app/App.js
+++ src/app/App.js
@@ -6,20 +6,28 @@ import Main from './layouts/Main'
 import NavBar from './components/ui/NavBar'
 import { ToastContainer } from 'react-toastify'
 import { ProfessionProvider } from './hooks/useProfession'
+import { QualitiesProvider } from './hooks/useQualities'
 
 function App() {
     return (
         <div>
             <NavBar />
+            {/* <QualitiesProvider> */}
             <ProfessionProvider>
-                <Switch>
-                    <Route path="/users/:userId?/:edit?" component={Users} />
-                    <Route path="/login/:type?" component={Login} />
-                    <Route path="/" exact component={Main} />
-                    <Redirect to="/" />
-                </Switch>
+                <QualitiesProvider>
+                    <Switch>
+                        <Route
+                            path="/users/:userId?/:edit?"
+                            component={Users}
+                        />
+                        <Route path="/login/:type?" component={Login} />
+                        <Route path="/" exact component={Main} />
+                        <Redirect to="/" />
+                    </Switch>
+                </QualitiesProvider>
             </ProfessionProvider>
             <ToastContainer />
+            {/* </QualitiesProvider> */}
         </div>
     )
 }
--- src/app/components/ui/qualities/QualitiesList.jsx
+++ src/app/components/ui/qualities/QualitiesList.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Qualitie from './Qualitie'
+import { useQualities } from '../../../hooks/useQualities'
 
 const QualitiesList = ({ qualities }) => {
+    console.log('qualities', qualities)
+    const { isLoading } = useQualities()
+    if (isLoading) return 'Loading...'
     return (
         <>
             {qualities.map((qual) => (
-                <Qualitie key={qual._id} {...qual} />
+                <Qualitie key={qual} id={qual} />
             ))}
         </>
     )
